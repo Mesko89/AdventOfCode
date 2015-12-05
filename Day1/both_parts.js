3c0bbc102b5ff6401101cad1d45c9703b9d81f17
Mesko <me@mesko.co>
2015-12-05 14:45:30 +0100

    Day 1 solutions

---
+++ Day1/both_parts.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var fs = require('fs');
+
+var input = fs.readFileSync('Day1/puzzle_input').toString().replace(/\r|\n/g,'');
+var floorInfo = { basementPosition: null, floor: 0 };
+floorInfo = input.split('').reduce(toFloorInfo, floorInfo);
+
+console.log('Instructions take santa to ' + floorInfo.floor + 'th floor.');
+console.log(
+	'Santa goes into basement by following instruction at position ' +
+	floorInfo.basementPosition +
+	'.'
+);
+
+function toFloorInfo(floorInfo, c, i) {
+	if (c === '(') floorInfo.floor++;
+	else floorInfo.floor--;
+	if (floorInfo.basementPosition === null && floorInfo.floor < 0)
+		floorInfo.basementPosition = i + 1;
+	return floorInfo;
+}
